ed596fb0bebbb683592b499ca50ca1dbb14a353e
MollyMeini <mxue@takima.fr>
2023-02-08 16:27:04 +0100

    i successfully show the data in the page

--- src/main/java/io/takima/master3/AddClasseJspController.java
+++ src/main/java/io/takima/master3/AddClasseJspController.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * <p>
@@ -38,20 +39,42 @@ import java.io.IOException;
 @SuppressWarnings("serial")
 @WebServlet("/AddClasse")
 public class AddClasseJspController extends HttpServlet {
+    static String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
 
-    HelloService helloService = new HelloService();
+    static String PAGE_FOOTER = "</body></html>";
 
     @Override
     public void init() {
         System.out.println("Servlet initialized successfully");
     }
-
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //this.doGet(req, resp);
+        String classeName = req.getParameter("classname");
+        int member = Integer.parseInt(req.getParameter("member"));
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println(PAGE_HEADER);
+        writer.println("<h1>" + classeName + "</h1>");
+        writer.println("<h1>" + member + "</h1>");
+        writer.println(PAGE_FOOTER);
+        writer.close();
+        //if classname exist in the database we report repeat error, otherwise we add into the DB
+        if(classeName == null){
 
-            req.setAttribute("helloService", helloService);
+        }else{
+
+        }
+
+    }    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            //Search all the info of the class through
+            //put the get info into the Request
+            //req.setAttribute("helloService", helloService);
+            // turn to the add_classe page
             RequestDispatcher dispatcher = req.getRequestDispatcher("/add_classe.jsp");
             dispatcher.forward(req, resp);
+
     }
 
 }
